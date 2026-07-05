030e22cf2ad4b5a3fb645ca127eda12cb3c96812
Marccel Janara <nengahyuichiro34@gmail.com>
2026-07-05 20:24:02 +0700

    Add project files.

---
+++ Models/Category.cs
@@ -0,0 +1,7 @@
+﻿namespace TodoApp.Models
+{
+    public class Category
+    {
+
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace TodoApp.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Username { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Password { get; set; } = null!;
+        public DateTime Created_At { get; set; }
+    }
+}
